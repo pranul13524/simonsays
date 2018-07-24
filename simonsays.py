7ec94e6a0b1a3f02b4d611e1d58b4574a9eec643
pl82524 <pl82524@eanesisd.net>
2018-07-24 14:11:29 -0500

    randomly blinks R, G, B, or Y

--- simonsays.py
+++ simonsays.py
@@ -1,17 +1,23 @@
+import RPi.GPIO as GPIO
+import LEDRGB as LED
 import time
 import random
-colors = ['R', 'G', 'B', 'Y']
 
-def append_list():
-	n = random.randint(0, 3)
-	colors.append(colors[n])
-	color_string = ''.join(colors)
-	for i in range(0, len(colors)):
-		print colors[i].lower()
-		time.sleep(1)
+colors = [ 'R', 'G', 'B', 'Y' ]
+R_pin = 11
+G_pin = 12
+B_pin = 13
+LED.setup(R_pin, G_pin, B_pin)
 
+def loop():
+	while True:
+		n = random.randint(0,3)
+		LED.setColor(colors[n])
+		time.sleep(0.5)
+		LED.noColor()
+		time.sleep(0.5)
 if __name__ == '__main__':
 	try:
-		append_list()
+		loop()
 	except KeyboardInterrupt:
-		print 'Goodbye'
+		LED.destroy()
